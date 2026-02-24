a86e961738cf6cb5bd2584b505bcb4d869201e1d
JonathanLab <jonathanmieloo@gmail.com>
2026-02-24 17:12:10 +0100

    yup more stuff

---
+++ apps/web/src/components/FlowDiagram.jsx
@@ -0,0 +1,16 @@
+export default function FlowDiagram() {
+  return (
+    <div
+      className="flow-diagram-placeholder"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      DIAGRAM HERE
+    </div>
+  );
+}
