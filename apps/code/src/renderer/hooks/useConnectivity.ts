97eea414ed80bdca55cb34f54800425c425c79b9
Vojta Bartoš <vojtech.b@posthog.com>
2026-04-29 12:03:50 +0200

    chore(code): replace no-connection dialog with debounced toast (#1936)

--- apps/code/src/renderer/hooks/useConnectivity.ts
+++ apps/code/src/renderer/hooks/useConnectivity.ts
@@ -3,9 +3,7 @@ import { useConnectivityStore } from "@stores/connectivityStore";
 export function useConnectivity() {
   const isOnline = useConnectivityStore((s) => s.isOnline);
   const isChecking = useConnectivityStore((s) => s.isChecking);
-  const showPrompt = useConnectivityStore((s) => s.showPrompt);
   const check = useConnectivityStore((s) => s.check);
-  const dismiss = useConnectivityStore((s) => s.dismissPrompt);
 
-  return { isOnline, isChecking, showPrompt, check, dismiss };
+  return { isOnline, isChecking, check };
 }
